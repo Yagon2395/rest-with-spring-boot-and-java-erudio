5aca1d30e99973d37a4b485200207305cba10505
Yago Nunes <yagon2395@gmail.com>
2022-12-29 10:13:46 -0300

    generating project with spring

---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/restwithspringbootandjavaerudio/RestWithSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restwithspringbootandjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
